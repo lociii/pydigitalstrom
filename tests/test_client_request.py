e595d8d40b4f58f689257728132ba6def52b29f5
Jens Nistler <opensource@jensnistler.de>
2019-11-17 16:24:09 +0000

    fix formatting

--- tests/test_client_request.py
+++ tests/test_client_request.py
@@ -2,8 +2,7 @@
 import aiounittest
 from aioresponses import aioresponses
 
-from pydigitalstrom.exceptions import DSCommandFailedException, \
-    DSRequestException
+from pydigitalstrom.exceptions import DSCommandFailedException, DSRequestException
 from tests.common import get_testclient
 
 
@@ -16,45 +15,52 @@ class TestClientRawRequest(aiounittest.AsyncTestCase):
     async def test_raw_request(self):
         client = get_testclient()
         with aioresponses() as mock_get:
-            mock_get.get(url='https://dss.local:8080/json/hello',
-                         payload=dict(ok=True, ping='pong'))
-            response = await client.raw_request(url='/json/hello')
-            self.assertEqual(response, dict(ok=True, ping='pong'))
+            mock_get.get(
+                url="https://dss.local:8080/json/hello",
+                payload=dict(ok=True, ping="pong"),
+            )
+            response = await client.raw_request(url="/json/hello")
+            self.assertEqual(response, dict(ok=True, ping="pong"))
 
     async def test_raw_request_not_200(self):
         client = get_testclient()
         with aioresponses() as mock_get:
-            mock_get.get(url='https://dss.local:8080/json/hello', status=400)
+            mock_get.get(url="https://dss.local:8080/json/hello", status=400)
             with self.assertRaises(DSRequestException):
-                await client.raw_request(url='/json/hello')
+                await client.raw_request(url="/json/hello")
 
     async def test_raw_request_ok_missing(self):
         client = get_testclient()
         with aioresponses() as mock_get:
-            mock_get.get(url='https://dss.local:8080/json/hello',
-                         payload=dict(ping='pong'))
+            mock_get.get(
+                url="https://dss.local:8080/json/hello", payload=dict(ping="pong")
+            )
             with self.assertRaises(DSCommandFailedException):
-                await client.raw_request(url='/json/hello')
+                await client.raw_request(url="/json/hello")
 
     async def test_raw_request_ok_false(self):
         client = get_testclient()
         with aioresponses() as mock_get:
-            mock_get.get(url='https://dss.local:8080/json/hello',
-                         payload=dict(ok=False, ping='pong'))
+            mock_get.get(
+                url="https://dss.local:8080/json/hello",
+                payload=dict(ok=False, ping="pong"),
+            )
             with self.assertRaises(DSCommandFailedException):
-                await client.raw_request(url='/json/hello')
+                await client.raw_request(url="/json/hello")
 
     async def test_raw_request_invalid_json(self):
         client = get_testclient()
         with aioresponses() as mock_get:
-            mock_get.get(url='https://dss.local:8080/json/hello',
-                         body=b'{whatever is not json')
+            mock_get.get(
+                url="https://dss.local:8080/json/hello", body=b"{whatever is not json"
+            )
             with self.assertRaises(DSRequestException):
-                await client.raw_request(url='/json/hello')
+                await client.raw_request(url="/json/hello")
 
 
 class TestClientRequest(aiounittest.AsyncTestCase):
     """
     TODO: implement tests for DSClient.request
     """
+
     pass
