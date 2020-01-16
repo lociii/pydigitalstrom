b6cfb8c5d23c512dc0eed527deda89a0a4a70ce5
Jens Nistler <opensource@jensnistler.de>
2020-01-16 15:58:55 +0000

    Bump version: 1.0.1 → 1.1.0

--- pydigitalstrom/__init__.py
+++ pydigitalstrom/__init__.py
@@ -1,7 +1,7 @@
 """python api client for digitalSTROM"""
 
 __title__ = "pydigitalstrom"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __url__ = "https://github.com/lociii/pydigitalstrom"
 __author__ = "Jens Nistler"
 __author_email__ = "opensource@jensnistler.de"
